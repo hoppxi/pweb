03616d4c31869b783165e38f4fdedfa33b325bc1
hoppxi <hoppxii@gmail.com>
2025-08-14 12:17:05 +0300

    simplify

--- data.js
+++ data.js
@@ -1,21 +1,20 @@
-
 const blogData = [
   {
     id: 1,
-    title: "From Windows to Hyprland: How Linux and Ricing Saved Me",
+    title: "test article 1",
     content: "../blog-contents/1.html",
     date: "2025-07-14",
     type: "blog",
-    image: "../assets/wallpaper1.jpg"
+    image: "../assets/wallpaper1.jpg",
   },
   {
     id: 2,
-    title: "The Power of QML, AGS, Fabric, and More",
+    title: "test article 2",
     content: "../blog-contents/2.html",
     date: "2025-07-10",
     type: "github",
-    image: "../assets/wallpaper1.jpg"
-  }
+    image: "../assets/wallpaper1.jpg",
+  },
 ];
 
 export default blogData;
